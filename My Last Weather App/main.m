3cf4447f42a6a858edc8c2f73279dcac9f85b6eb
Akbar Nurlybayev <akbar@architech.ca>
2015-04-06 10:27:06 -0400

    Initial Commit

---
+++ My Last Weather App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Last Weather App
+//
+//  Created by Akbar Nurlybayev on 2015-04-06.
+//  Copyright (c) 2015 Akbar Nurlybayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ My Last Weather AppTests/My_Last_Weather_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  My_Last_Weather_AppTests.m
+//  My Last Weather AppTests
+//
+//  Created by Akbar Nurlybayev on 2015-04-06.
+//  Copyright (c) 2015 Akbar Nurlybayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface My_Last_Weather_AppTests : XCTestCase
+
+@end
+
+@implementation My_Last_Weather_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
